558b6b5899fb07ad6f7f52ddabaa93e2c56fadb5
James Smith <james@floppy.org.uk>
2008-08-20 18:07:21 +0100

    Make meter into an Observable object

--- lib/currentcost/meter.rb
+++ lib/currentcost/meter.rb
@@ -1,11 +1,14 @@
 require 'rb232'
 require 'rb232/text_protocol'
 require 'currentcost/reading'
+require 'observer'
 
 module CurrentCost
   
   class Meter
-    
+
+    include Observable
+
     def initialize(port = '/dev/ttyS0')
       @port = RB232::Port.new(port, :baud_rate => 9600)
       @protocol = RB232::TextProtocol.new(@port, "\n")
@@ -14,7 +17,11 @@ module CurrentCost
     end
 
     def update(message)
+      # Parse reading from message
       @latest_reading = Reading.from_xml(message) unless message.nil?
+      # Inform observers
+      changed
+      notify_observers(@latest_reading)
     rescue CurrentCost::ParseError
       nil
     end
--- spec/meter_spec.rb
+++ spec/meter_spec.rb
@@ -1,9 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-TEST_PORT = "/dev/ttyUSB0"
+TEST_PORT = "/dev/ttyS0"
 
 describe CurrentCost::Meter do
-  
+
   before(:each) do    
     @meter = CurrentCost::Meter.new(TEST_PORT)
   end
@@ -13,11 +13,40 @@ describe CurrentCost::Meter do
   end
 
   it "should allow access to the latest reading" do
-    @meter.latest_reading.is_a?(CurrentCost::Reading).should be_true
+    @meter.should respond_to(:latest_reading)
   end
   
   it "should be an Observer" do
     @meter.should respond_to(:update)
+  end
+
+  it "should be Observable" do
+    @meter.is_a?(Observable).should be_true
+  end
+
+  # A mock observer to test meter observable code
+  class TestObserver
+    def initialize
+      @received_reading = false
+    end
+    def update(reading)
+      @received_reading = true if reading.is_a?(CurrentCost::Reading)
+    end
+    attr_reader :received_reading
+  end
+
+  it "should send reading to observers when a valid reading is received" do
+    t = TestObserver.new
+    @meter.add_observer(t)
+    @meter.update('<msg><date><dsb>00007</dsb><hr>23</hr><min>14</min><sec>57</sec></date><src><name>CC02</name><id>00077</id><type>1</type><sver>1.06</sver></src><ch1><watts>00365</watts></ch1><ch2><watts>00000</watts></ch2><ch3><watts>00000</watts></ch3><tmpr>23.3</tmpr></msg>')
+    t.received_reading.should be_true
+  end
+
+  it "should not notify observers when an invalid reading is received" do
+    t = TestObserver.new
+    @meter.add_observer(t)
+    @meter.update('')
+    t.received_reading.should be_false
   end
 
 end
