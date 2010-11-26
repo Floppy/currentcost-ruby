e07285e8ee4f4a81046d2ed47a22f4ae6d269878
James Smith <james@floppy.org.uk>
2010-11-26 18:31:50 +0000

    Add an example that dumps history data

---
+++ examples/dump_history.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+dir = File.dirname(__FILE__) + '/../lib'
+$LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
+
+require 'rubygems'
+require 'currentcost/meter'
+
+require 'optparse'
+
+# Command-line options
+options = {:port => '/dev/ttyS0'}
+OptionParser.new do |opts|
+  opts.on("-p", "--serial_port SERIAL_PORT", "serial port") do |p|
+    options[:port] = p
+  end  
+end.parse!
+
+# A simple observer class which will receive updates from the meter
+class HistoryObserver
+  def update(xml)
+    if xml.include?('<hist>')
+      puts xml
+    end
+  end
+end
+
+# Create meter
+meter = CurrentCost::Meter.new(options[:port], :cc128 => true)
+# Create observer
+observer = HistoryObserver.new
+# Register observer with meter
+meter.protocol.add_observer(observer)
+# Wait a while, let things happen
+sleep(60)
+# Close the meter object to stop it receiving data
+meter.close
